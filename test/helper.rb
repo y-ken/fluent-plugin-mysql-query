91a820d0a282405271f44dedb140fe9b60f10494
okkez <okkez000@gmail.com>
2017-03-29 10:35:39 +0900

    Cleanup

--- test/helper.rb
+++ test/helper.rb
@@ -1,28 +1,13 @@
-require 'rubygems'
-require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
+require 'bundler/setup'
 require 'test/unit'
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
+$LOAD_PATH.unshift(__dir__)
 require 'fluent/test'
-unless ENV.has_key?('VERBOSE')
-  nulllogger = Object.new
-  nulllogger.instance_eval {|obj|
-    def method_missing(method, *args)
-      # pass
-    end
-  }
-  $log = nulllogger
-end
-
+require 'fluent/test/helpers'
 require 'fluent/plugin/in_mysql_query'
 
 class Test::Unit::TestCase
+  include Fluent::Test::Helpers
+  extend Fluent::Test::Helpers
 end
