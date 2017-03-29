46de3d31a99117d1fa5b0fcdbd00db3ba3654b01
okkez <okkez000@gmail.com>
2017-03-29 10:56:28 +0900

    Organize test code

--- test/helper.rb
+++ test/helper.rb
@@ -5,6 +5,7 @@ $LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
 $LOAD_PATH.unshift(__dir__)
 require 'fluent/test'
 require 'fluent/test/helpers'
+require 'fluent/test/driver/input'
 require 'fluent/plugin/in_mysql_query'
 
 class Test::Unit::TestCase
--- test/plugin/test_in_mysql_query.rb
+++ test/plugin/test_in_mysql_query.rb
@@ -14,27 +14,32 @@ class MysqlQueryInputTest < Test::Unit::TestCase
     record_hostname yes
   ]
 
-  def create_driver(conf=CONFIG,tag='test')
-    Fluent::Test::OutputTestDriver.new(Fluent::MysqlQueryInput, tag).configure(conf)
+  def create_driver(conf=CONFIG)
+    Fluent::Test::Driver::Input.new(Fluent::Plugin::MysqlQueryInput).configure(conf)
   end
 
-  def test_configure
-    assert_raise(Fluent::ConfigError) {
-      d = create_driver('')
-    }
-    d = create_driver %[
-      host            localhost
-      port            3306
-      interval        30
-      tag             input.mysql
-      query           SHOW VARIABLES LIKE 'Thread_%'
-      record_hostname yes
-    ]
-    assert_equal 'localhost', d.instance.host
-    assert_equal 3306, d.instance.port
-    assert_equal 30, d.instance.interval
-    assert_equal 'input.mysql', d.instance.tag
-    assert_equal true, d.instance.record_hostname
-    assert_equal false, d.instance.nest_result
+  sub_test_case "configure" do
+    test "empty" do
+      assert_raise(Fluent::ConfigError) do
+        create_driver('')
+      end
+    end
+
+    test "simple" do
+      d = create_driver %[
+        host            localhost
+        port            3306
+        interval        30
+        tag             input.mysql
+        query           SHOW VARIABLES LIKE 'Thread_%'
+        record_hostname yes
+      ]
+      assert_equal 'localhost', d.instance.host
+      assert_equal 3306, d.instance.port
+      assert_equal 30, d.instance.interval
+      assert_equal 'input.mysql', d.instance.tag
+      assert_equal true, d.instance.record_hostname
+      assert_equal false, d.instance.nest_result
+    end
   end
 end
